67019acead2be3e6373563c9996bce603d60a15a
David <sambunjak.david@gmail.com>
2025-08-23 04:05:11 +0200

    Implemented a simple rps game via console

--- script.js
+++ script.js
@@ -4,4 +4,49 @@ function getComputerChoice() {
   return options[randomNumber];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  return prompt("Rock, paper, or scissors?");
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  console.log(humanChoice);
+  console.log(computerChoice);
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    return;
+  } else if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      console.log("You lose!");
+      computerScore++;
+      return;
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "scissors") {
+      console.log("You lose!");
+      computerScore++;
+      return;
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "rock") {
+      console.log("You lose!");
+      computerScore++;
+      return;
+    }
+  }
+  console.log("You win!");
+  humanScore++;
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(`Human score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
+  }
+}
+
+playGame();
