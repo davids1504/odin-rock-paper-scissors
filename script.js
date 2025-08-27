a8694495d7278dedf3f630f14ad8916a5cb0705f
David <sambunjak.david@gmail.com>
2025-08-27 21:40:47 +0200

    Finish base UI and game flow
    script.js file has been edited so it accepts the game flow.
    declareWinner() function is used to track the game flow, and the game
    ends if either player or CPU get to 5 wins. Current win/lose ratio and
    status is shown on the screen.

--- script.js
+++ script.js
@@ -1,70 +1,88 @@
+let humanScore = 0;
+let computerScore = 0;
+let isGameOver = false;
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   const options = ["rock", "paper", "scissors"];
   return options[randomNumber];
 }
 
-function getHumanChoice() {
-  return prompt("Rock, paper, or scissors?");
+function callPlayRound(evt) {
+  let playerChoice = evt.target.textContent;
+
+  playRound(playerChoice, getComputerChoice());
 }
 
-let humanScore = 0;
-let computerScore = 0;
+const buttons = document.querySelectorAll(".buttons button");
+buttons.forEach((el) => {
+  el.addEventListener("click", callPlayRound);
+});
+
+const resultPara = document.createElement("p");
+let currentScore = document.createElement("p");
+const gameInfo = document.querySelector(".game-info");
 
 function playRound(humanChoice, computerChoice) {
+  resultPara.remove();
   humanChoice = humanChoice.toLowerCase();
-  console.log(humanChoice);
-  console.log(computerChoice);
+  resultPara.textContent = `You: ${humanChoice.toUpperCase()} ~ CPU: ${computerChoice.toUpperCase()}: `;
+
+  function updateScore() {
+    currentScore.textContent = `You: ${humanScore} ~ CPU: ${computerScore}`;
+    declareWinner();
+    if (isGameOver) {
+      buttons.forEach((el) => {
+        el.removeEventListener("click", callPlayRound);
+      });
+    }
+  }
+
+  function declareWinner() {
+    if (humanScore === 5) {
+      alert("GAME OVER! YOU WIN!\nPlease refresh page to play again.");
+      isGameOver = true;
+    } else if (computerScore === 5) {
+      alert("GAME OVER! CPU WINS!\nPlease refresh page to play again.");
+      isGameOver = true;
+    }
+  }
+
+  updateScore();
+
+  gameInfo.appendChild(resultPara);
+  gameInfo.appendChild(currentScore);
+
   if (humanChoice === computerChoice) {
-    console.log("It's a tie!");
+    resultPara.textContent += "It's a tie!";
     return;
   } else if (humanChoice === "rock") {
     if (computerChoice === "paper") {
-      console.log("You lose!");
+      resultPara.textContent += "You lose!";
+
       computerScore++;
+      updateScore();
       return;
     }
   } else if (humanChoice === "paper") {
     if (computerChoice === "scissors") {
-      console.log("You lose!");
+      resultPara.textContent += "You lose!";
+
       computerScore++;
+      updateScore();
       return;
     }
   } else if (humanChoice === "scissors") {
     if (computerChoice === "rock") {
-      console.log("You lose!");
+      resultPara.textContent += "You lose!";
+
       computerScore++;
+      updateScore();
       return;
     }
   }
-  console.log("You win!");
+  resultPara.textContent += "You win!";
+
   humanScore++;
+  updateScore();
 }
-
-// function playGame() {
-//   playRound(getHumanChoice(), getComputerChoice());
-//   console.log(`Human score: ${humanScore}`);
-//   console.log(`Computer score: ${computerScore}`);
-// }
-
-const buttonsContainer = document.querySelector(".buttons");
-buttonsContainer.addEventListener("click", (evt) => {
-  let playerChoice = "";
-  switch (evt.target.textContent) {
-    case "Rock":
-      playerChoice = "Rock";
-      break;
-    case "Paper":
-      playerChoice = "Paper";
-      break;
-    case "Scissors":
-      playerChoice = "Scissors";
-      break;
-    default:
-      alert("Unexpected Error");
-  }
-
-  playRound(playerChoice, getComputerChoice());
-});
-
-// playGame();
